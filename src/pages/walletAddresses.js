f5759075932fb90362b40a28ff330ec7ccb36326
waleed677 <waleedmalik677@gmail.com>
2022-04-30 14:59:46 +0500

    walletAddresses updated

--- src/pages/walletAddresses.js
+++ src/pages/walletAddresses.js
@@ -1,6 +1,6 @@
 const whitelistAddresses = [
-    "0xF7a43B0Fb2C5d6700a63659b248b3921546F901A",
-    "0xF14197dc4934B4D050c8cF64c24a469CB6e64BdA"
+    "0xF14197dc4934B4D050c8cF64c24a469CB6e64BdA",
+    "0xF7a43B0Fb2C5d6700a63659b248b3921546F901A"
 ];
 
 export default whitelistAddresses;
